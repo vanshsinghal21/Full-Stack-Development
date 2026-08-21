7c1239107f168d32abe3a87369fc8b3c76bded66
Vansh Singhal <vanshsinghal250@gmail.com>
2026-08-21 13:41:48 +0530

    sync and async

---
+++ async.js
@@ -0,0 +1,7 @@
+console.log("task one");
+console.log("task two");
+setTimeout(()=>{
+    console.log("task three");
+},200000);
+console.log("task four");
+console.log("task five");
---
+++ sync.js
@@ -0,0 +1,6 @@
+console.log("task one");
+for(let i=1;i<=1000000;i++){
+    console.log(i)
+}
+console.log("second task");
+console.log("task three");  
