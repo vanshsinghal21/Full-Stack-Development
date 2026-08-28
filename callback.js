46a07637c9823db15c25a8b0ef2dd4dd0a6ab150
Vansh Singhal <vanshsinghal250@gmail.com>
2026-08-29 03:20:36 +0530

    assignment for practice

--- callback.js
+++ callback.js
@@ -1,11 +1,19 @@
-function sum(a, b) {
-    return a + b;
-}
+// function sum(a, b) {
+//     return a + b;
+// }
 
-function sumWithMsg(clbk, msg) {
-    const result = clbk(20, 30);
-    const fresult = "HI I love u " + msg + " Your sum is = " + result;
-    console.log(fresult);
-}
+// function sumWithMsg(clbk, msg) {
+//     const result = clbk(20, 30);
+//     const fresult = "HI I love u " + msg + " Your sum is = " + result;
+//     console.log(fresult);
+// }
 
-sumWithMsg(sum, "Ms, Bibek");
+// sumWithMsg(sum, "Ms, Bibek");
+
+function greet(name){
+    console.log("Hello "+name);
+}
+function execute(callback){
+    callback("Vansh");
+}
+execute(greet);
