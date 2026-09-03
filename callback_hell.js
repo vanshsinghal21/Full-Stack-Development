8be5d024c6b878996f6cc5e31b36d237a18bbe0e
Vansh Singhal <vanshsinghal250@gmail.com>
2026-09-03 09:37:07 +0530

    Real Callback hell

--- callback_hell.js
+++ callback_hell.js
@@ -1,20 +1,33 @@
-function test1(){
+function test1(cb){
     setTimeout(()=>{
         console.log(("test1"))
+        cb();
     },4000);
 }
-function test2(){
+function test2(cb){
     setTimeout(()=>{
         console.log(("test2"))
+        cb();
     },2000);
 }
-function test3(){
-    
+function test3(cb){
+    setTimeout(()=>{
+        console.log(("test3"))
+        cb();
+    },200);
 }
-function test4(){
+function test4(cb){
+    setTimeout(()=>{
+        console.log(("test4"))
+        cb();
+    },1000);
+}
 
-}
-test1();
-test2();
-test3();
-test4();
+test1(()=>{
+    test2(()=>{
+        test3(()=>{
+            test4();
+        });
+    });
+});
+
