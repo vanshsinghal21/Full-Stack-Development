960917278c1b8662615aa5bca54aab63e5be1e85
Vansh Singhal <vanshsinghal250@gmail.com>
2026-09-03 09:29:40 +0530

    Callback_hell_1

---
+++ callback_hell.js
@@ -0,0 +1,20 @@
+function test1(){
+    setTimeout(()=>{
+        console.log(("test1"))
+    },4000);
+}
+function test2(){
+    setTimeout(()=>{
+        console.log(("test2"))
+    },2000);
+}
+function test3(){
+    
+}
+function test4(){
+
+}
+test1();
+test2();
+test3();
+test4();
